86e8b92783258cc0ebedd8c2cfe082c2f18e552a
Atharva Patil <atharva.patil2024@gmail.com>
2024-02-04 15:02:41 +0530

    react travel website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from "./pages/Layout";
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import BlogsDetails from './pages/BlogsDetails'
+import PlacesRoute from './pages/PlacesRoute'
+import About from './pages/About'
+import Nopage from './pages/Nopage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="blogs/:id" element={<BlogsDetails />} />
+            <Route path="best-places" element={<PlacesRoute />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={< Nopage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
